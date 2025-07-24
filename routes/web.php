eee28c5830cd94944d26ee6f4217cf5d545455c6
lalanf21 <lalanfathurrahman@gmail.com>
2025-07-24 16:15:49 +0700

    Fix issue routing

--- routes/web.php
+++ routes/web.php
@@ -17,23 +17,10 @@ use Inertia\Inertia;
 |
 */
 
-Route::middleware('guest')->group(function () {
-    Route::get('/login', fn () => Inertia::render('Auth/Login'))->name('login');
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::get('/notes/{note}', [NoteController::class, 'show'])->name('notes.show');
-});
-
-Route::get('/', [NoteController::class, 'index'])->name('notes.index');
 
 Route::middleware('auth')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    });
-
     Route::get('/notes/create', fn () => Inertia::render('Notes/CreateNote'));
-
-    
+    Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/notes', [NoteController::class, 'store'])->name('notes.store');
     Route::get('/notes/{note}/edit', [NoteController::class, 'edit'])->name('notes.edit');
     Route::put('/notes/{note}', [NoteController::class, 'update'])->name('notes.update');
@@ -41,7 +28,14 @@ Route::middleware('auth')->group(function () {
     Route::post('/notes/{note}/share', [NoteController::class, 'share'])->name('notes.share');
     Route::post('/notes/{note}/comment', [NoteController::class, 'comment'])->name('notes.comment');
     Route::delete('/notes/{note}', [NoteController::class, 'destroy'])->name('notes.destroy');
-
 });
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', fn () => Inertia::render('Auth/Login'))->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
+Route::get('/', [NoteController::class, 'index'])->name('notes.index');
+Route::get('/notes/{note}', [NoteController::class, 'show'])->name('notes.show');
 
 require __DIR__.'/auth.php';
